4bfb16fba668c3d098203b6765c9011cdcdcbd86
Carlos V <77943334+valdcarl@users.noreply.github.com>
2023-10-28 14:12:00 -0600

    added jest, babel packages, added files to where functions are to be made and added the test files for those functions to be tested

---
+++ js/capitalize.js
@@ -0,0 +1,16 @@
+// capitalize.js
+
+function capitalize(string) {
+    /**
+     * string: parameter of a string to be passed to capitlize
+     * returns: the same 'string' with a Capital letter
+     */
+    let s = string[0].toUpperCase();                // uppercase to the first char of string
+    for (let i = 1; i < string.length; i++) {       // append the remaining letters of string to s
+        s += string[i];
+    }
+    return s;
+}
+
+
+module.exports = capitalize;
---
+++ test/capitalize.test.js
@@ -0,0 +1,7 @@
+// capitalize.test.js
+
+const capitalize = require("../js/capitalize");
+
+test("Captilize Test 1", () => {
+    expect(capitalize("carlos")).toBe("Carlos");
+});
