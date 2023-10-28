65ea639f6900dfe6bb76f403496a2429bee4b800
Carlos V <77943334+valdcarl@users.noreply.github.com>
2023-10-28 14:20:32 -0600

    Completed reverseString function and testing

--- js/reverseString.js
+++ js/reverseString.js
@@ -1,1 +1,14 @@
 // reverseString.js
+
+function reverseString(string) {
+    /**
+     * string: a string to be reversed
+     * returns: the string but in reverse order
+     * 
+     * ex: string = "hello"     return: "olleh"
+     */
+    return string.split("").reverse().join("");
+    
+}
+
+module.exports = reverseString;
---
+++ test/reverseString.test.js
@@ -0,0 +1,7 @@
+// reverseString.test.js
+
+const reverseString = require("../js/reverseString");
+
+test("Reverse String Test 1", () => {
+    expect(reverseString("carlos")).toBe("solrac");
+});
